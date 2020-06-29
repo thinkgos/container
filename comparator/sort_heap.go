d9eff6418f100269450922cb472874e66e7b541a
mo <thinkgo@aliyun.com>
2020-06-29 10:53:10 +0800

    fix comparator

--- comparator/sort_heap.go
+++ comparator/sort_heap.go
@@ -1,3 +1,17 @@
+// Copyright [2020] [thinkgos]
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package comparator
 
 import (
@@ -10,6 +24,7 @@ var (
 	_ sort.Interface = (*Container)(nil)
 )
 
+// Container for sort or heap sort
 type Container struct {
 	noCopy  noCopy
 	Items   []interface{}
@@ -17,14 +32,17 @@ type Container struct {
 	Reverse bool
 }
 
+// Len implement heap.Interface
 func (sf *Container) Len() int {
 	return len(sf.Items)
 }
 
+// Swap implement heap.Interface
 func (sf *Container) Swap(i, j int) {
 	sf.Items[i], sf.Items[j] = sf.Items[j], sf.Items[i]
 }
 
+// Less implement heap.Interface
 func (sf *Container) Less(i, j int) bool {
 	if sf.Reverse {
 		i, j = j, i
@@ -36,10 +54,12 @@ func (sf *Container) Less(i, j int) bool {
 	return Compare(sf.Items[i], sf.Items[j]) < 0
 }
 
+// Push implement heap.Interface
 func (sf *Container) Push(x interface{}) {
 	sf.Items = append(sf.Items, x)
 }
 
+// Pop implement heap.Interface
 func (sf *Container) Pop() interface{} {
 	old := sf.Items
 	n := len(old)
@@ -48,6 +68,7 @@ func (sf *Container) Pop() interface{} {
 	return x
 }
 
+// Sort sorts values into ascending sequence according to their natural ordering, or according to the provided comparator.
 func (sf *Container) Sort() {
 	sort.Sort(sf)
 }
